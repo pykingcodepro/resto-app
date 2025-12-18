fff7e43ad08070522bcf3a4bd4f7c5732dad3851
Vatsal Sejpal <sejpalvatsal456@gmail.com>
2025-12-18 11:57:17 +0530

    Added headers and footers

---
+++ app/_components/RestaurantFooter.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RestaurantFooter() {
+  return (
+    <div className='bg-black text-white fixed bottom-0 w-screen text-center h-15 flex items-center justify-center'>
+      <p>All rights are reserved by &copy; Resto App 2025.</p>
+    </div>
+  )
+}
---
+++ app/_components/RestaurantHeader.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+export default function RestaurantHeader() {
+  return (
+    <div className="flex justify-between mb-5">
+      <div className="logo">
+        <img
+          className="w-20 rounded-full"
+          src="https://marketplace.canva.com/EAFpeiTrl4c/2/0/1600w/canva-abstract-chef-cooking-restaurant-free-logo-a1RYzvS1EFo.jpg"
+          alt="logo"
+        />
+      </div>
+
+      <ul className="flex w-100 justify-around items-center">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/">Login/SignUp</Link>
+        </li>
+        <li>
+          <Link href="/">Profile</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
