3471c059232b71b2a4f1a58c253397b0fb518043
Vatsal <yourhelper258@gmail.com>
2026-01-11 18:11:44 +0530

    Added frontend for user login with email

--- app/user/auth/page.tsx
+++ app/user/auth/page.tsx
@@ -1,10 +1,12 @@
 import RestaurantHeader from '@/app/_components/RestaurantHeader'
+import UserLogin from '@/app/_components/userAuthComponents/UserLogin'
 import React from 'react'
 
 export default function Home() {
   return (
     <>
       <RestaurantHeader isLogin={true} />  { /* Temparary using restaurant header */ }
+      <UserLogin />
     </>
   )
 }
